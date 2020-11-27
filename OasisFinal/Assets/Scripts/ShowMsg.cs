e553ac13e045dc6231fc3d1d0c06482e63a361d8
DianaCN <47123599+DianaCN@users.noreply.github.com>
2020-11-27 12:26:03 -0600

    Letrero
    Ya sale un letrero transparente

---
+++ OasisFinal/Assets/Scripts/ShowMsg.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMsg : MonoBehaviour
+{
+    public float distanceToShow;
+    public Renderer msgRender;
+
+    void Update()
+    {
+        msgRender.enabled = Vector3.Distance(Camera.main.transform.position, transform.position) < distanceToShow;
+    }
+}
